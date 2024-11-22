582f9859e428287a44849c87013292f318395f20
Alexandre Magno <xandemag@gmail.com>
2024-11-21 21:39:53 -0300

    WIP

--- main.go
+++ main.go
@@ -11,17 +11,16 @@ func main() {
 
 	fmt.Print("Digite o diâmetro da bola: ")
 	fmt.Scan(&diameter)
-
-	fmt.Print("Digite as dimensões da caixa (altura, largura, comprimento): ")
-	_, err := fmt.Scanf("%d,%d,%d", &height, &width, &length)
-	if err != nil {
-		fmt.Println("Erro ao ler as dimensões da caixa:", err)
-		return
-	}
-
 	ball, err := src.NewBall(diameter)
 	if err != nil {
 		fmt.Println("Erro ao criar a bola:", err)
+		return
+	}
+
+	fmt.Print("Digite as dimensões da caixa com virgulas (altura, largura, comprimento): ")
+	_, err = fmt.Scanf("%d,%d,%d", &height, &width, &length)
+	if err != nil {
+		fmt.Println("Erro ao ler as dimensões da caixa:", err)
 		return
 	}
 
